1f15717bc16ea92a634929c6815ae62fa9674f80
Aidan <aidanfromprogramming@gmail.com>
2022-06-30 16:10:10 -0400

    initial commit
    Signed-off-by: Aidan <aidanfromprogramming@gmail.com>

--- main.py
+++ main.py
@@ -107,6 +107,7 @@ async def update(ctx):
         await ctx.send('```\n' + res + '```')
         # Run pip update on requirements.txt
         res = os.popen("pip install -r requirements.txt").read()
+        print(res)
         await ctx.send('```\n' + res + '```')
         await msg.edit(content="Restarting...")
         # await ctx.bot.get_command('restart').callback(self, ctx)
