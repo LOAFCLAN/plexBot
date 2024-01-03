a96c707245940b5933e651c5b7f2b5934fe7032c
JayFromProgramming <ajsweene@mtu.edu>
2024-01-03 00:21:46 -0500

    Added a way for plexbot to detect a failure of the thumbnail webserver

--- cogs/plexEvents.py
+++ cogs/plexEvents.py
@@ -61,7 +61,7 @@ class PlexEvents(Cog):
             return
         channel_id = table.get_row(guild_id=guild.id)['channel_id']
         logging.debug(f"Starting event listener for {guild.name} ({plex.friendlyName})")
-        self.bot.loop.create_task(self.start_event_listener(guild.id, channel_id))
+        # self.bot.loop.create_task(self.start_event_listener(guild.id, channel_id))
 
     @EventDecorator.on_event('plex_disconnect')
     async def on_plex_disconnect(self, plex):
